e85f380004667ab10ae9b82378ccc9f44b260bd8
Saaket Poray <saaketporay@gmail.com>
2019-10-23 20:28:30 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,5 +1,21 @@
 const { Schema } = require('mongoose');
 
-const User = new Schema({});
+const User = new Schema({
+    firstName: String,
+    lastName: String,
+    preferredName: String,
+    year: String,
+    email: String,
+    cellNumber: String,
+    netID: String,
+    ccID: String,
+    major: [String],
+    interests: [String],
+    github: String,
+    discord: String,
+    linkedin: String,
+    bio: String
+});
+
 
 module.exports = User;
